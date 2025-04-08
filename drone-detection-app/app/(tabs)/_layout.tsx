93805e05c4636ad6ba4d5803c97e59d05e56a9ec
Emil Albrechtsen <145019248+ealbrechtsen@users.noreply.github.com>
2025-04-08 10:58:36 +0200

    feat: update tab layout with Entypo icons and modify index text. Also created props for ts type

--- drone-detection-app/app/(tabs)/_layout.tsx
+++ drone-detection-app/app/(tabs)/_layout.tsx
@@ -1,37 +1,39 @@
-import FontAwesome from '@expo/vector-icons/Entypo';
+import Entypo from '@expo/vector-icons/Entypo';
 import { Tabs } from 'expo-router';
+import type { TabBarIconProps } from '../../types/navigation.d.ts';
+
 
 export default function TabLayout() {
-  return (
-    <Tabs screenOptions={{ tabBarActiveTintColor: 'blue' }}>
-      <Tabs.Screen
-        name="index"
-        options={{
-          title: 'Home',
-          tabBarIcon: ({ color }) => <FontAwesome size={28} name="home" color={color} />,
-        }}
-      />
-      <Tabs.Screen
-        name="map"
-        options={{
-          title: 'Map',
-          tabBarIcon: ({ color }) => <FontAwesome size={28} name="map" color={color} />,
-        }}
-      />
-      <Tabs.Screen
-        name="statistic"
-        options={{
-          title: 'Statistic',
-          tabBarIcon: ({ color }) => <FontAwesome size={28} name="bar-graph" color={color} />,
-        }}
-      />
-      <Tabs.Screen
-        name="more"
-        options={{
-          title: 'More',
-          tabBarIcon: ({ color }) => <FontAwesome size={28} name="menu" color={color} />,
-        }}
-      />
-    </Tabs>
-  );
+    return (
+        <Tabs screenOptions={{ tabBarActiveTintColor: 'blue' }}>
+            <Tabs.Screen
+                name="index"
+                options={{
+                    title: 'Home',
+                    tabBarIcon: ({ color }: TabBarIconProps) => (<Entypo size={28} name="home" color={color} />),
+                }}
+            />
+            <Tabs.Screen
+                name="map"
+                options={{
+                    title: 'Map',
+                    tabBarIcon: ({ color }: TabBarIconProps) => (<Entypo size={28} name="map" color={color} />),
+                }}
+            />
+            <Tabs.Screen
+                name="statistic"
+                options={{
+                    title: 'Statistic',
+                    tabBarIcon: ({ color }: TabBarIconProps) => (<Entypo size={28} name="bar-graph" color={color} />),
+                }}
+            />
+            <Tabs.Screen
+                name="more"
+                options={{
+                    title: 'More',
+                    tabBarIcon: ({ color }: TabBarIconProps) => (<Entypo size={28} name="menu" color={color} />),
+                }}
+            />
+        </Tabs>
+    );
 }
