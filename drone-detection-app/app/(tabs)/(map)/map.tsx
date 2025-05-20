e0283834ac5ed03c4165c351971115d37fda3bfe
Emil Albrechtsen <145019248+ealbrechtsen@users.noreply.github.com>
2025-05-20 20:27:35 +0200

    feat: integrate react-native-maps for map visualization and update notification handler

--- drone-detection-app/app/(tabs)/(map)/map.tsx
+++ drone-detection-app/app/(tabs)/(map)/map.tsx
@@ -1,9 +1,28 @@
-import { View, Text } from "react-native";
+import { StyleSheet, View, Dimensions } from "react-native";
+import MapView from "react-native-maps";
 
 export default function MapTab() {
   return (
-    <View>
-      <Text>Her kommer kortvisningen</Text>
+    <View style={styles.container}>
+      <MapView
+        style={styles.map}
+        initialRegion={{
+          latitude: 55.6761, 
+          longitude: 12.5683,
+          latitudeDelta: 0.05,
+          longitudeDelta: 0.05,
+        }}
+      />
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  map: {
+    width: Dimensions.get("window").width,
+    height: Dimensions.get("window").height,
+  },
+});
--- drone-detection-app/src/lib/localNotifications.ts
+++ drone-detection-app/src/lib/localNotifications.ts
@@ -1,29 +1,31 @@
-// import * as Notifications from "expo-notifications";
+import * as Notifications from "expo-notifications";
 
-// export const setNotificationHandler = async () => {
-//     Notifications.setNotificationHandler({
-//         handleNotification: async () => ({
-//             shouldShowAlert: true,
-//             shouldPlaySound: false,
-//             shouldSetBadge: false
-//         }),
-//     });
-// }
+export const setNotificationHandler = async () => {
+    Notifications.setNotificationHandler({
+        handleNotification: async () => ({
+            shouldShowAlert: true,
+            shouldPlaySound: false,
+            shouldSetBadge: false,
+            shouldShowBanner: true,
+            shouldShowList: true
+        }),
+    });
+}
 
-// export const requestNotificationPermissions = async () => {
-//     const { status } = await Notifications.requestPermissionsAsync();
-//     if (status !== "granted") {
-//         alert("Permission denied!");
-//     }
-// };
+export const requestNotificationPermissions = async () => {
+    const { status } = await Notifications.requestPermissionsAsync();
+    if (status !== "granted") {
+        alert("Permission denied!");
+    }
+};
 
-// // call scheduleNotificationAsync()
-// export const scheduleNotification = async (title: string, body: string, seconds: number) => {
-//     await Notifications.scheduleNotificationAsync({
-//         content: {
-//             title,
-//             body,
-//         },
-//         trigger: { type: Notifications.SchedulableTriggerInputTypes.TIME_INTERVAL, seconds },
-//     });
-// };
+// call scheduleNotificationAsync()
+export const scheduleNotification = async (title: string, body: string, seconds: number) => {
+    await Notifications.scheduleNotificationAsync({
+        content: {
+            title,
+            body,
+        },
+        trigger: { type: Notifications.SchedulableTriggerInputTypes.TIME_INTERVAL, seconds },
+    });
+};
